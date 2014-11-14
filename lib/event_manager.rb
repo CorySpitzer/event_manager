e92707ed4bb2ea071ff0186e1d346a5db3bf7e4e
CorySpitzer <coryspitzer@gmail.com>
2014-11-14 09:44:03 -0500

    Fix typo in attendee name. Use csv library to parse data.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,10 @@
-
+require 'csv'
 puts "EventManager initialized!"
 
-path = "/home/coriander/code/event_manager/event_attendees.csv"
-lines = File.readlines(path)
-lines.each_with_index do |line, index|
-    next if index == 0
-    columns = line.split(',')
-    name = columns[2]
-    puts name
+contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
+contents.each do |row|
+    name = row[:first_name]
+    zipcode = row[:zipcode]
+    puts name, zipcode
     #puts '-----------'
 end
