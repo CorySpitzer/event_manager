6ca9b568c6489ed9c9c67224d824a4bc7e003bd4
CorySpitzer <coryspitzer@gmail.com>
2014-11-14 10:01:53 -0500

    Add zeros to zipcodes to make them 5 digits.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,6 +5,13 @@ contents = CSV.open("event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
     name = row[:first_name]
     zipcode = row[:zipcode]
+    # If zipcode is blank, put in 00000
+    # else if not 5 digits pad the front with 0's 
+    zc_length = zipcode.to_s.length
+    unless zc_length == 5
+        # add leading 0's until it has 5 digits
+        zipcode = '0'*(5-zc_length) + zipcode.to_s
+    end
     puts name, zipcode
     #puts '-----------'
 end
