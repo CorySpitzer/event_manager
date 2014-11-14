10e6debe5b1b37fc237c6d73dd78d86a3cb1205e
CorySpitzer <coryspitzer@gmail.com>
2014-11-14 10:21:41 -0500

    Refine zipcode fixing: Add case for zipcodes more than 5 digits. Initialize zipcode variable as a string to avoid converting it many times.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -4,13 +4,17 @@ puts "EventManager initialized!"
 contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
 contents.each do |row|
     name = row[:first_name]
-    zipcode = row[:zipcode]
+    zipcode = row[:zipcode].to_s
     # If zipcode is blank, put in 00000
     # else if not 5 digits pad the front with 0's 
-    zc_length = zipcode.to_s.length
-    unless zc_length == 5
+    zc_length = zipcode.length
+    if zc_length > 5
+        # Truncate it to first 5 digits
+        zipcode = zipcode[0..4]
+    elsif zc_length < 5
         # add leading 0's until it has 5 digits
-        zipcode = '0'*(5-zc_length) + zipcode.to_s
+        #zipcode = zipcode.rjust(5, '0') # this fails for an empty zip
+        zipcode = '0'*(5-zc_length) + zipcode
     end
     puts name, zipcode
     #puts '-----------'
