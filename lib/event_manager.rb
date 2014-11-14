cda36dcf8a9ded0c7a20e4f790b632df5a0720c1
CorySpitzer <coryspitzer@gmail.com>
2014-11-14 10:30:10 -0500

    Add check for nil zipcode as separate case and use rjust to add zeros to short zipcodes.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,13 +8,15 @@ contents.each do |row|
     # If zipcode is blank, put in 00000
     # else if not 5 digits pad the front with 0's 
     zc_length = zipcode.length
-    if zc_length > 5
+    if zipcode.nil?
+        zipcode = '0'*5
+    elsif zc_length > 5
         # Truncate it to first 5 digits
         zipcode = zipcode[0..4]
     elsif zc_length < 5
         # add leading 0's until it has 5 digits
-        #zipcode = zipcode.rjust(5, '0') # this fails for an empty zip
-        zipcode = '0'*(5-zc_length) + zipcode
+        zipcode = zipcode.rjust(5, '0') # this fails for empty zip: nil
+        #zipcode = '0'*(5-zc_length) + zipcode
     end
     puts name, zipcode
     #puts '-----------'
