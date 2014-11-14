9ad12a444aa381b39749cacaaacc69775708f568
CorySpitzer <coryspitzer@gmail.com>
2014-11-14 09:37:04 -0500

    Print first names.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,12 @@
+
 puts "EventManager initialized!"
 
 path = "/home/coriander/code/event_manager/event_attendees.csv"
 lines = File.readlines(path)
-lines.each do |line|
-    puts line
+lines.each_with_index do |line, index|
+    next if index == 0
+    columns = line.split(',')
+    name = columns[2]
+    puts name
+    #puts '-----------'
 end
